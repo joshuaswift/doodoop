aed6c66d01236a9ac483609e63fc20009ad9c12c
Joshua Swift <joshuaswift@outlook.com>
2020-10-08 11:01:50 +0100

    add home screen navigation

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,39 @@
 import React from "react";
 import { Button } from 'react-bootstrap';
 import DooDoopHeader from '../molecules/doodoop-header';
+import { useHistory } from "react-router-dom";
+
 
 export default function JoinQuiz() {
+  let history = useHistory();
+
+  function createQuiz() {
+    history.push("/create-quiz");
+  }
+
+  function joinQuiz() {
+    history.push("/join-quiz");
+  }
+
   return (
     <>
       <DooDoopHeader />
-      <Button size="lg" block variant="primary" type="submit">
+      <Button
+        size="lg"
+        block
+        variant="primary"
+        type="submit"
+        onClick={createQuiz}
+      >
         Create quiz
       </Button>
-      <Button size="lg" block variant="primary" type="submit">
+      <Button
+        size="lg"
+        block
+        variant="primary"
+        type="submit"
+        onClick={joinQuiz}
+      >
         Join quiz
       </Button>
     </>
