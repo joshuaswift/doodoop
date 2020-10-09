12affad89494d7b334b53cd55fb9611c2287c248
Joshua Swift <joshuaswift@outlook.com>
2020-10-09 09:04:13 +0100

    add button transitions

--- src/pages/admin-quiz.js
+++ src/pages/admin-quiz.js
@@ -33,6 +33,45 @@ const NEXT_SONG_MUTATION = gql`
   }
 `;
 
+const PLAY_CURRENT_SONG_MUTATION = gql`
+  mutation getGameSessions($id: Int!) {
+    gameSessionsMutations {
+      startPlayingCurrentSong(id: $id) {
+        name
+        id
+        status
+        points
+        link
+      }
+    }
+  }
+`;
+const STOP_CURRENT_SONG_MUTATION = gql`
+  mutation getGameSessions($id: Int!) {
+    gameSessionsMutations {
+      stopPlayingCurrentSong(id: $id) {
+        name
+        id
+        status
+        points
+        link
+      }
+    }
+  }
+`;
+
+const FINISH_QUIZ_MUTATION = gql`
+  mutation getGameSessions($id: Int!) {
+    gameSessionsMutations {
+      startNextSong(id: $id) {
+        id
+      }
+    }
+  }
+`;
+
+
+
 
 export default function AdminQuiz() {
   const { id } = useParams();
@@ -40,29 +79,74 @@ export default function AdminQuiz() {
   const { loading, error, data, refetch } = useQuery(GAME_SESSION_QUERY, {
     variables: { id: parseInt(id) },
   });
+  console.log("CL: AdminQuiz -> loading", loading);
   const [startNextSong] = useMutation(NEXT_SONG_MUTATION, {
     onCompleted: refetch,
   });
+  const [playCurrentSong] = useMutation(PLAY_CURRENT_SONG_MUTATION, {
+    onCompleted: refetch,
+  });
+  const [stopCurrentSong] = useMutation(STOP_CURRENT_SONG_MUTATION, {
+    onCompleted: refetch,
+  });
+  const [finishQuiz] = useMutation(FINISH_QUIZ_MUTATION);
 
   if (loading) return null;
   if (error) return `Error! ${error}`;
 
-  const {name, enterCode} = data.gameSessions[0];
+  const {name, enterCode, currentRoundElement} = data.gameSessions[0];
 
-  const playOrStopSong = (event) => {
+  const playSong = (event) => {
     event.preventDefault();
-
-    setPlaying(!playing);
-    
-    if (playing === false) {
-      
-    }
-    //update round element status if stopping
+    playCurrentSong({ variables: { id: parseInt(id) } });
+    setPlaying(true);
+  };
+  const stopSong = (event) => {
+    event.preventDefault();
+    stopCurrentSong({ variables: { id: parseInt(id) } });
+    setPlaying(false);
   };
   const nextSong = (event) => {
     event.preventDefault();
-    startNextSong({ variables: { id: id } });
+    startNextSong({ variables: { id: parseInt(id) } });
   };
+
+  function ButtonDisplay(props) {
+    if (props.status === "pending") {
+      return (
+        <Button size="lg" variant="primary" type="submit" onClick={nextSong}>
+          Next Song
+        </Button>
+      );
+    } else if (props.status === "started") {
+      return (
+        <Button
+          className="ml-3"
+          size="lg"
+          variant="primary"
+          type="submit"
+          onClick={playSong}
+        >
+          Play Song
+        </Button>
+      );
+    } else if (props.status === "playing") {
+      return (
+        <Button
+          className="ml-3"
+          size="lg"
+          variant="primary"
+          type="submit"
+          onClick={stopSong}
+        >
+          Stop Song
+        </Button>
+      );
+    } else {
+      return null;
+    }
+
+  }
 
 
   return (
@@ -71,6 +155,11 @@ export default function AdminQuiz() {
       <Header name={name} />
       <Card className="mb-3">
         <Card.Body>Join code: {enterCode}</Card.Body>
+      </Card>
+      <Card className="mb-3">
+        <Card.Body>
+          Round: {currentRoundElement ? currentRoundElement.name : ""}
+        </Card.Body>
       </Card>
       <Form>
         <Container>
@@ -81,38 +170,28 @@ export default function AdminQuiz() {
               </ListGroup>
             </Col>
             <Col md="9">
-              {
-              playing ? 
-              <ListGroup>
-                <ListGroup.Item>Answer 1</ListGroup.Item>
-              </ListGroup> 
-              : null
-              }
+              {playing ? (
+                <ListGroup>
+                  <ListGroup.Item>Answer 1</ListGroup.Item>
+                </ListGroup>
+              ) : null}
             </Col>
           </Row>
         </Container>
         <div className="float-right mt-5">
-          <Button
-            size="lg"
-            variant="primary"
-            type="submit"
-            onClick={nextSong}
-          >
-            Next Song
-          </Button>
-          <Button
-            className="ml-3"
-            size="lg"
-            variant="primary"
-            type="submit"
-            onClick={playOrStopSong}
-          >
-            {playing ? "Stop Song" : "Play Song"}
-          </Button>
+          <ButtonDisplay
+            status={
+              currentRoundElement ? currentRoundElement.status : "pending"
+            }
+          />
         </div>
       </Form>
       <ReactPlayer
-        url="https://www.youtube.com/watch?v=ZyhrYis509A"
+        url={
+          currentRoundElement
+            ? currentRoundElement.link
+            : "https://www.youtube.com/watch?v=ZyhrYis509A"
+        }
         playing={playing}
         className="d-none"
       />
